c2b9f07b78482cbe4d7fdd8f2ee10c9605c8727a
dyu <david.yu.ftw@gmail.com>
2018-02-15 18:06:05 +0800

    layout

--- examples/layout.cpp
+++ examples/layout.cpp
@@ -2,37 +2,51 @@
 #include <nana/gui/widgets/label.hpp>
 
 static const char LAYOUT[] =
-"<main margin=2 grid=[2,1] gap=2>"
+//"<main_ margin=2 grid=[2,1] gap=2>"
+//"<left_>|70%<right_>"
+//"<left_><right_ weight=70%>"
+"vert margin=5"
+"<header_ weight=20"
+  "<top_>"
+">"
+"<content_"
+  "<left_><right_>"
+  ">"
+"<footer_ weight=20"
+  "<bottom_>"
+">"
 ;
 
-namespace ui {
-    nana::label& label(const std::string& text, nana::form& fm)
-    {
-        auto l = new nana::label(fm);
-        l->caption(text);
-        return *l;
-    }
-}
+static const char* LINKS[] = {
+    "foo",
+    "bar",
+    "baz"
+};
 
 int main(int argc, char* argv[])
 {
-    nana::form fm;
+    nana::form fm{nana::API::make_center(1005,710)};
+    
     fm.caption("Layout example");
     fm.bgcolor(nana::color_rgb(0xFFFFFF));
     
-    nana::place place(fm);
+    nana::place place{fm};
     place.div(LAYOUT);
     
-    nana::label left(fm);
-    left.caption("left");
+    nana::label
+        top{fm, "top"},
+        bottom{fm, "bottom"},
+        left{fm, "left"},
+        right{fm, "right"};
     
-    place["main"] << left;
-    //place["main"] << ui::label("left", fm);
+    top.text_align(nana::align::center);
+    bottom.text_align(nana::align::center);
     
-    nana::label right(fm);
-    right.caption("right");
+    place["left_"] << left;
+    place["right_"] << right;
     
-    place["main"] << right;
+    place["top_"] << top;
+    place["bottom_"] << bottom;
     
     place.collocate();
     fm.show();

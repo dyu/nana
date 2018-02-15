eeea7fb0980ce46eb77d4f4b6ddf0c8b44e5e3a4
dyu <david.yu.ftw@gmail.com>
2018-02-15 17:08:08 +0800

    left/right

--- examples/layout.cpp
+++ examples/layout.cpp
@@ -1,14 +1,42 @@
 #include <nana/gui/wvl.hpp>
 #include <nana/gui/widgets/label.hpp>
 
-int main()
+static const char LAYOUT[] =
+"<main margin=2 grid=[2,1] gap=2>"
+;
+
+namespace ui {
+    nana::label& label(const std::string& text, nana::form& fm)
+    {
+        auto l = new nana::label(fm);
+        l->caption(text);
+        return *l;
+    }
+}
+
+int main(int argc, char* argv[])
 {
     nana::form fm;
     fm.caption("Layout example");
     fm.bgcolor(nana::color_rgb(0xFFFFFF));
     
     nana::place place(fm);
+    place.div(LAYOUT);
     
+    nana::label left(fm);
+    left.caption("left");
+    
+    place["main"] << left;
+    //place["main"] << ui::label("left", fm);
+    
+    nana::label right(fm);
+    right.caption("right");
+    
+    place["main"] << right;
+    
+    place.collocate();
     fm.show();
     nana::exec();
+    
+    return 0;
 }
